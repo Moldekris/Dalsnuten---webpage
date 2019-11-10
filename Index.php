4d189622192f1b64c70aff88ec828939393c52e6
Moldekris <55106216+Moldekris@users.noreply.github.com>
2019-11-10 12:54:27 +0100

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+Created using JS Bin
+http://jsbin.com
+
+Copyright (c) 2019 by anonymous (http://jsbin.com/cebedivara/1/edit)
+
+Released under the MIT license: http://jsbin.mit-license.org
+-->
+<meta name="robots" content="noindex">
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>CSS Basics</title>
+  
+  
+    
+</head>
+  </body>
+  
+     <h1> Hiking to Dalsnuten, Sandnes </h1>
+
+<h2>Time: 2 hours - return trip</h2>
+<h2>Length: 3,8 km </h2>
+
+ <p> The country walk from Dale northwards will take about 4 hours for a round trip. These pathways require good walking shoes. There are good opportunities for bathing at Bymarka and Dalevatn.<p>
+
+   <img src="https://no.regionstavanger-ryfylke.com/imageresizer/?image=%2Fdbimgs%2Futsikten-fra-dalsnuten-1750x656-foto-grim-berge-natural-light-earth.jpg&action=Background_Overlay" alt="Mountain top"width="550"
+
+   <a href="contact.html">contact</a>    
+   <a href="hiking details.html">hiking details</a>    
+   <a href="map.html">map</a> 
+
+   </body>
+HTML
+        
+        
+     
+     
+</html>
